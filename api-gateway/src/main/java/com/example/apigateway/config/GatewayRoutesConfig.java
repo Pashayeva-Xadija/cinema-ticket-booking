9e3c0d63e0932a8c446902c118a8ac19e8bd2a68
Pashayeva-Xadija <xadijapashayeva@gmail.com>
2025-09-28 16:36:59 +0400

    fix: exclude auth docs from auth-api route

--- api-gateway/src/main/java/com/example/apigateway/config/GatewayRoutesConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/config/GatewayRoutesConfig.java
@@ -1,3 +1,4 @@
+// api-gateway/src/main/java/com/example/apigateway/config/GatewayRoutesConfig.java
 package com.example.apigateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -12,22 +13,25 @@ public class GatewayRoutesConfig {
     public RouteLocator routes(RouteLocatorBuilder b) {
         return b.routes()
 
-
                 .route("auth-docs", r -> r
-                        .order(-1)
+                        .order(-10)
                         .path(
                                 "/api/auth/swagger-ui/**",
                                 "/api/auth/swagger-ui.html",
                                 "/api/auth/v3/api-docs",
                                 "/api/auth/v3/api-docs/**"
                         )
-                        .filters(f -> f.stripPrefix(2))   
+                        .filters(f -> f.stripPrefix(2))  // /api/auth/... -> /...
                         .uri("https://energetic-celebration-production.up.railway.app")
                 )
 
-
+                
                 .route("auth-api", r -> r
+                        .order(0)
                         .path("/api/auth/**")
+                        .and().not(p -> p.path("/api/auth/v3/api-docs/**"))
+                        .and().not(p -> p.path("/api/auth/swagger-ui/**"))
+                        .and().not(p -> p.path("/api/auth/swagger-ui.html"))
                         .filters(f -> f.stripPrefix(2).prefixPath("/auth"))
                         .uri("https://energetic-celebration-production.up.railway.app")
                 )
