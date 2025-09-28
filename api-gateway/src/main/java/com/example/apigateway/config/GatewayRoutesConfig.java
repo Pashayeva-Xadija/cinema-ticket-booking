70d1c2c209d86976bd5b7c46ea1db79c55679f5a
Pashayeva-Xadija <xadijapashayeva@gmail.com>
2025-09-28 15:13:49 +0400

    Add auth service domain to gateway routes

--- api-gateway/src/main/java/com/example/apigateway/config/GatewayRoutesConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/config/GatewayRoutesConfig.java
@@ -5,21 +5,28 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class GatewayRoutesConfig {
 
     @Bean
     public RouteLocator routes(RouteLocatorBuilder b) {
+        String AUTH = "https://SENIN-AUTH-DOMENIN.up.railway.app";
+
         return b.routes()
+
                 .route("auth-docs", r -> r.path(
                                 "/api/auth/v3/api-docs",
                                 "/api/auth/swagger-ui/**",
                                 "/api/auth/swagger-ui.html")
-                        .filters(f -> f.stripPrefix(2))
-                        .uri("https://AUTH-APP-DOMENIN.railway.app"))
+                        .filters(f -> f.stripPrefix(2)) 
+                        .uri(AUTH))
+
+
                 .route("auth-api", r -> r.path("/api/auth/**")
                         .filters(f -> f.stripPrefix(2).prefixPath("/auth"))
-                        .uri("https://AUTH-APP-DOMENIN.railway.app"))
+                        .uri(AUTH))
+
                 .build();
     }
 }
