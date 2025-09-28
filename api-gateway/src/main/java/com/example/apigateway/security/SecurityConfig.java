844dbb251db3a8f0e1e592478597be65bb5dd445
Pashayeva-Xadija <xadijapashayeva@gmail.com>
2025-09-28 17:23:55 +0400

    disable basic auth; allow index.html

--- api-gateway/src/main/java/com/example/apigateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.apigateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
 @Configuration
 public class SecurityConfig {
 
@@ -12,20 +14,15 @@ public class SecurityConfig {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .cors(cors -> {})
+
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
                 .authorizeExchange(ex -> ex
+                        .pathMatchers("/", "/index.html", "/app.js", "/styles.css", "/favicon.ico").permitAll()
                         .pathMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .pathMatchers("/api/*/swagger-ui.html", "/api/*/swagger-ui/**", "/api/*/v3/api-docs/**").permitAll()
                         .pathMatchers("/api/auth/**").permitAll()
                         .pathMatchers("/actuator/**").permitAll()
-                        // --- Demo üçün public GET-lər:
-                        .pathMatchers(org.springframework.http.HttpMethod.GET,
-                                "/api/showtime/**",
-                                "/api/inventory/**",
-                                "/api/booking/**",
-                                "/api/payment/**",
-                                "/api/ticket/**",
-                                "/api/notification/**"
-                        ).permitAll()
                         .anyExchange().authenticated()
                 )
                 .build();
