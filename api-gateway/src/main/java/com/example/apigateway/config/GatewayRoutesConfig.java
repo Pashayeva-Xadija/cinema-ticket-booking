8e63b50d6b8f4d90393c2ee9461d18ee7edf958b
Pashayeva-Xadija <xadijapashayeva@gmail.com>
2025-09-28 16:24:17 +0400

    fix: route order for auth swagger & api-docs

--- api-gateway/src/main/java/com/example/apigateway/config/GatewayRoutesConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/config/GatewayRoutesConfig.java
@@ -1,4 +1,3 @@
-
 package com.example.apigateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -14,17 +13,24 @@ public class GatewayRoutesConfig {
         return b.routes()
 
 
-                .route("auth-docs", r -> r.path(
+                .route("auth-docs", r -> r
+                        .order(-1)
+                        .path(
                                 "/api/auth/swagger-ui/**",
                                 "/api/auth/swagger-ui.html",
-                                "/api/auth/v3/api-docs/**",
-                                "/api/auth/v3/api-docs")
-                        .filters(f -> f.stripPrefix(2))
-                        .uri("https://energetic-celebration-production.up.railway.app"))
-                
-                .route("auth-api", r -> r.path("/api/auth/**")
+                                "/api/auth/v3/api-docs",
+                                "/api/auth/v3/api-docs/**"
+                        )
+                        .filters(f -> f.stripPrefix(2))   
+                        .uri("https://energetic-celebration-production.up.railway.app")
+                )
+
+
+                .route("auth-api", r -> r
+                        .path("/api/auth/**")
                         .filters(f -> f.stripPrefix(2).prefixPath("/auth"))
-                        .uri("https://energetic-celebration-production.up.railway.app"))
+                        .uri("https://energetic-celebration-production.up.railway.app")
+                )
 
                 .build();
     }
